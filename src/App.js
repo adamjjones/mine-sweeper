e2111ca0c0f43ee05cc23903d528c80db39733ad
Adam Jones <adam@linuxtampa.com>
2019-08-24 15:02:44 -0400

    Finished API calls`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Gameboard from './components/Gameboard'
-import Populateboard from './components/Populateboard'
 
 class App extends Component {
   render() {
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
-import Populateboard from './Populateboard'
 
 export class Gameboard extends Component {
-  state = {
-    board: []
+  constructor() {
+    super()
+    this.getCurrentGameState = this.getCurrentGameState.bind(this)
+    this.state = {
+      board: [],
+      id: 0
+    }
   }
 
+  // console.log('1')
   componentDidMount() {
-    // console.log('1')
+    this.createNewGame()
+  }
+  createNewGame() {
     Axios({
       method: 'post',
       url: 'http://minesweeper-api.herokuapp.com/games'
     }).then(result => {
       // console.log('3')
       this.setState({
-        board: result.data.board
+        board: result.data.board,
+        id: result.data.id
+      })
+    })
+  }
+
+  postAction(action, row, col) {
+    Axios({
+      method: 'post',
+      url: `http://minesweeper-api.herokuapp.com/games/${this.state.id}/${action}`,
+      data: {
+        id: this.state.id,
+        row: row,
+        col: col
+      }
+    }).then(result => {
+      this.setState({
+        board: result.data.board,
+        id: result.data.id,
+        state: result.data.state,
+        mines: result.data.mines
+      })
+    })
+  }
+
+  getCurrentGameState() {
+    console.log('getCurrentGameState called', this)
+    Axios({
+      method: 'get',
+      url: `http://minesweeper-api.herokuapp.com/games/${this.state.id}`
+    }).then(result => {
+      console.log(result)
+      this.setState({
+        id: result.data.id,
+        board: result.data.board,
+        state: result.data.state,
+        mines: result.data.mines
       })
     })
   }
@@ -23,11 +66,28 @@ export class Gameboard extends Component {
   createCell = () => {
     // console.log('4')
     console.log(this.state.board)
+    let countRow = 0
     const rows = this.state.board.map(row => {
+      let countCell = -1
       const cells = row.map(value => {
-        return <td className="cell">{value}</td>
+        countCell++
+        return (
+          <td
+            key={`cell-${countCell}`}
+            row={countRow}
+            col={countCell}
+            className="cell"
+          >
+            {value}
+          </td>
+        )
       })
-      return <tr className="row">{cells}</tr>
+      countRow += 1
+      return (
+        <tr key={`row-${countRow}`} className="row">
+          {cells}
+        </tr>
+      )
     })
     return rows
   }
@@ -39,6 +99,7 @@ export class Gameboard extends Component {
         <table className="table">
           <tbody>{this.createCell()}</tbody>
         </table>
+        <button onClick={this.getCurrentGameState}>Get Game State</button>
       </div>
     )
   }
